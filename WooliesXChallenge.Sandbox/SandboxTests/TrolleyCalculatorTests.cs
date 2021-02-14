7eda4f5426d23228f4ec4bb4822f3fe9bf02b212
Simon Ng <mercury_sn@hotmail.com>
2021-02-14 13:28:42 +1100

    Refactoring the tests and code for attempts to solve the exert trolleyCalculator into the Sandbox project

--- WooliesXChallenge.Sandbox/SandboxTests/TrolleyCalculatorTests.cs
+++ WooliesXChallenge.Sandbox/SandboxTests/TrolleyCalculatorTests.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using WooliesXChallenge.Dto;
 using WooliesXChallenge.Dto.Request;
-using WooliesXChallenge.Services;
 using Xunit;
+using TrolleyCalculator = WooliesXChallenge.Sandbox.Service.TrolleyCalculator;
 
-namespace WooliesXChallenge.UnitTests.Services
+namespace WooliesXChallenge.Sandbox.SandboxTests
 {
     public class TrolleyCalculatorTests
     {
@@ -51,7 +51,7 @@ namespace WooliesXChallenge.UnitTests.Services
         [Fact]
         public void Test()
         {
-            List<int> numbers = new List<int>() { 1 };
+            List<int> numbers = new List<int>() { 3, 4, 5 };
             int target = 11;
             sum_up(numbers, target);
         }
@@ -68,7 +68,7 @@ namespace WooliesXChallenge.UnitTests.Services
 
             if (s == target)
                 Console.WriteLine("sum(" + string.Join(",", partial.ToArray()) + ")=" + target);
-            
+
 
             if (s >= target)
                 return;
@@ -89,7 +89,7 @@ namespace WooliesXChallenge.UnitTests.Services
         }
 
         [Fact]
-        public void Test2()
+        public void Test3()
         {
             List<int> numbers = new List<int>() { 1 };
             int target = 11;
@@ -130,5 +130,97 @@ namespace WooliesXChallenge.UnitTests.Services
 
             sum_up_reuse_recursive(numbers, target, toAdd);
         }
+
+
+        //[Fact]
+        public void CalculateCheapestTrolley_Test_2()
+        {
+            var trolley = new Trolley
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Test Product A", Price = 10}
+                },
+                Specials = new List<Special>
+                {
+                    new Special { Quantities = new List<Quantity>
+                        {
+                            new Quantity { Name = "Test Product A", quantity = 5}
+                        },
+                            Total = 5
+                        },
+                    new Special { Quantities = new List<Quantity>
+                        {
+                            new Quantity { Name = "Test Product A", quantity = 3}
+                        },
+                        Total = 3
+                    },
+                },
+                Quantities = new List<Quantity>
+                {
+                    new Quantity {Name = "Test Product A", quantity = 11}
+                }
+            };
+
+            var trolleyCalculator = new TrolleyCalculator();
+
+            var amount = trolleyCalculator.CalculateCheapestTrolley(trolley);
+
+            Assert.Equal(29m, amount);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            List<int> numbers = new List<int>() { 1 };
+            int target = 11;
+            sum_up(numbers, target);
+        }
+
+
+        
+
+        [Fact]
+        public void Test2()
+        {
+            List<int> numbers = new List<int>() { 1 };
+            int target = 11;
+            sum_up_reuse(numbers, target);
+
+            Assert.Equal(1, _answer.Count);
+            Assert.Equal(1, _answer[0][0]);
+            Assert.Equal(1, _answer[0][1]);
+            Assert.Equal(1, _answer[0][2]);
+            Assert.Equal(1, _answer[0][3]);
+            Assert.Equal(1, _answer[0][4]);
+        }
+
+        //private static void sum_up_reuse(List<int> numbers, int target)
+        //{
+        //    sum_up_reuse_recursive(numbers, target, new List<int>());
+        //}
+
+        //private static void sum_up_reuse_recursive(List<int> numbers, int target, List<int> toAdd)
+        //{
+        //    int sum = 0;
+        //    foreach (var add in toAdd)
+        //    {
+        //        sum += add;
+        //    }
+
+        //    if (sum == target)
+        //    {
+        //        _answer.Add(toAdd);
+        //    }
+
+        //    if (sum > target)
+        //    {
+        //        return;
+        //    }
+
+        //    toAdd.Add(numbers[0]);
+
+        //    sum_up_reuse_recursive(numbers, target, toAdd);
+        //}
     }
 }
---
+++ WooliesXChallenge.Sandbox/Service/TrolleyCalculator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using WooliesXChallenge.Dto;
+using WooliesXChallenge.Dto.Request;
+
+namespace WooliesXChallenge.Sandbox.Service
+{
+    public class TrolleyCalculator
+    {
+        public decimal CalculateCheapestTrolley(Trolley trolley)
+        {
+            return FindCheapestSpecial(trolley.Specials, trolley.Quantities, trolley.Products, int.MaxValue);
+
+        }
+
+        public decimal FindCheapestSpecial(List<Special> specials, List<Quantity> purchases, List<Product> basePrice,
+            int minAmount)
+        {
+            var minSpecialPrice = decimal.MaxValue;
+
+            foreach (var purchase in purchases)
+            {
+                foreach (var product in basePrice)
+                {
+                    specials.Add(new Special
+                    {
+                        Total = purchase.quantity * product.Price,
+                        Quantities = new List<Quantity>
+                        {
+                            new Quantity
+                            {
+                                Name = product.Name,
+                                quantity = purchase.quantity
+                            }
+                        }
+                    });
+                }
+
+                foreach (var special in specials)
+                {
+                    var specialPrice = 0.0m;
+
+                    foreach (var quantity in special.Quantities)
+                    {
+                        if (purchase.quantity >= quantity.quantity)
+                        {
+                            specialPrice = special.Total;
+
+                            if (specialPrice < minSpecialPrice)
+                            {
+                                minSpecialPrice = specialPrice;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return minSpecialPrice;
+        }
+
+        //public int FindCheapestSpecial(List<Special> specials, List<Quantity> purchases, List<Product> basePrice, int minAmount)
+        //{
+        //    if (purchases.Sum(p => p.quantity) == 0)
+        //    {
+        //        return minAmount;
+        //    }
+        //    else
+        //    {
+        //        foreach (var special in specials)
+        //        {
+        //            var thisSpecialAmount = 0;
+        //            foreach (var specialQuantity in special.Quantities)
+        //            {
+        //                foreach (var purchase in purchases)
+        //                {
+        //                    if (purchase.Name == specialQuantity.Name && purchase.quantity > specialQuantity.quantity)
+        //                    {
+        //                        thisSpecialAmount += special.Total;
+        //                        purchase.quantity = purchase.quantity - specialQuantity.quantity;
+        //                    }
+        //                    else
+        //                    {
+        //                        thisSpecialAmount = int.MaxValue;
+        //                    }
+        //                }
+        //            }
+
+        //            if (thisSpecialAmount < minAmount)
+        //            {
+        //                minAmount = thisSpecialAmount;
+        //            }
+        //        }
+
+        //        var canSpecialStillBeApplied = FindSpecialCanBeApplied(specials, purchases);
+
+        //        if (canSpecialStillBeApplied)
+        //        {
+        //            return FindCheapestSpecial(specials, purchases, basePrice, minAmount);
+        //        }
+
+                
+        //    }
+        //}
+
+        //private bool FindSpecialCanBeApplied(List<Special> specials, List<Quantity> purchases)
+        //{
+        //    foreach (var special in specials)
+        //    {
+        //        foreach (var quantity in special.Quantities)
+        //        {
+        //            foreach (var purchase in purchases)
+        //            {
+        //                if (purchase.quantity > quantity.quantity)
+        //                    return true;
+        //            }
+        //        }
+        //    }
+
+        //    return false;
+        //}
+    }
+}
